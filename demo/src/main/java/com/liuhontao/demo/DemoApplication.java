901ff1a3d2e5ce0b5211fa31c132fb6afa60b109
start-lht <start_lht@163.com>
2019-08-09 16:08:00 +0800

    aaa

---
+++ demo/src/main/java/com/liuhontao/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.liuhontao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
